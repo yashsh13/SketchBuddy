dfdf7fdd2afacd4e39114e339dab720c7f7a4f74
yashsh13 <yashpat1302@gmail.com>
2026-02-11 14:38:08 +0530

    removed localhost

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3001";
-export const WS_URL = process.env.WS_URL || "ws://localhost:8080";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+export const BACKEND_URL = process.env.BACKEND_URL;
+export const WS_URL = process.env.WS_URL;
+export const FRONTEND_URL = process.env.FRONTEND_URL;
