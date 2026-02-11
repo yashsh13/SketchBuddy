ea24068cf23e68752cbd36ae3f4cc50c390c2dd9
yashsh13 <yashpat1302@gmail.com>
2026-02-11 14:46:32 +0530

    Added NEXT_PUBLIC

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL;
-export const WS_URL = process.env.WS_URL;
-export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
